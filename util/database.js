ca3016fdf2613c685b58f25da71476eb2b0feb86
Red-Pillow <sahu5chetan5@gmail.com>
2023-10-28 18:38:38 +0530

    made changes to package.json and db dialect value

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-    dialect: process.env.DB_TYPE,
+    dialect: "mysql",
     host: process.env.DB_HOSTNAME
 });
 
